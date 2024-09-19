1ad9cf34c7629f29341fcbc525bd6cd0edbef358
makzzer <makzofx@gmail.com>
2024-09-19 18:40:03 -0300

    pequeño test de nextjs

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const Dashboard = () => {
+    return (
+        <h1>Hola soy el Dashboard</h1>
+        
+    )
+}
+
+export default Dashboard;
